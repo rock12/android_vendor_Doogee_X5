a293d51eea73865cb0bad9518261ae00007503a3
rock12 <s2012srock>
2017-10-26 15:32:28 +0200

    Fix

--- X5-vendor.mk
+++ X5-vendor.mk
@@ -1,10 +1,10 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/proprietary/bin,system/bin)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/etc,system/etc)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/proprietary/etc,system/etc)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/lib,system/lib)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/proprietary/lib,system/lib)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/vendor,system/vendor)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/proprietary/vendor,system/vendor)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/xbin,system/xbin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/Doogee/X5/proprietary/xbin,system/xbin)
 
